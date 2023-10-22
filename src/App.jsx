839663a238ff84bd2494ffc6ef332ad65bce82f4
Umair <umairshah0711@gmail.com>
2023-10-22 10:34:34 +0530

    Layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import './App.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Layout from "./pages/Layout";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
-  )
+    <RouterProvider router={router}>
+      <></>
+    </RouterProvider>
+  );
 }
 
-export default App
+export default App;
