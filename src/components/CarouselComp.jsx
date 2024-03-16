eac6160369c97267f2969d14ab2e4ee2d6e430d4
unknown <taabishshaikh2>
2024-03-16 13:54:38 +0530

    added fonts

--- src/components/CarouselComp.jsx
+++ src/components/CarouselComp.jsx
@@ -35,20 +35,20 @@ const CarouselComp = () => {
           <img src="/assets/SITES PHOTO/On Going Dombivali site/WhatsApp Image 2023-08-07 at 4.20.52 PM.jpeg" alt="" width={"100%"} height={650} />
           <div className="carousel-caption  ">
           
-            <p>  <h3 className="">Witness progress in action at our construction sites, where safety and precision converge to bring your vision to life. We take pride in creating environments that stand as a testament to quality craftsmanship</h3></p>
+            <p>  <h3 style={{ fontWeight:"bolder"}} className="">Witness progress in action at our construction sites, where safety and precision converge to bring your vision to life. We take pride in creating environments that stand as a testament to quality craftsmanship</h3></p>
           </div>
         </div>
         <div className="carousel-item">
           <img src="/assets/SITES PHOTO/mutha1.jpg" alt="" width={"100%"} height={650} />
           <div className="carousel-caption">
-            <h3 className="text-white">Our dedicated team, fueled by passion and expertise, transforms blueprints into reality. With a commitment to excellence, we ensure every project is a masterpiece of construction, blending innovation with traditional craftsmanship</h3>
+            <h3 className="" style={{ fontWeight:"bolder"}}>Our dedicated team, fueled by passion and expertise, transforms blueprints into reality. With a commitment to excellence, we ensure every project is a masterpiece of construction, blending innovation with traditional craftsmanship</h3>
             {/* <p>Some representative placeholder content for the second slide.</p> */}
           </div>
         </div>
         <div className="carousel-item">
           <img src="/assets/SITES PHOTO/silver.jpg"  alt="" width={"100%"} height={650} />
           <div className="carousel-caption ">
-            <h3>Explore the future of construction with our state-of-the-art machinery and innovative technologies. We embrace advancements to deliver projects with efficiency, sustainability, and a touch of modernity, setting new standards in the construction industry.</h3>
+            <h3 style={{ fontWeight:"bolder"}}>Explore the future of construction with our state-of-the-art machinery and innovative technologies. We embrace advancements to deliver projects with efficiency, sustainability, and a touch of modernity, setting new standards in the construction industry.</h3>
             {/* <p>Some representative placeholder content for the third slide.</p> */}
           </div>
         </div>
